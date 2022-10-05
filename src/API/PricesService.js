9bd90802ce4c3a2bd59500cb0bf229aebcfb15d5
Gassyr <gassyrdaulet@gmail.com>
2022-10-06 05:26:28 +0600

    fetch https

--- src/API/PricesService.js
+++ src/API/PricesService.js
@@ -5,14 +5,5 @@ export const getAllPrices = async (fromId) => {
   const { data } = await axios.post(serverURL, {
     fromId,
   });
-  fetch(serverURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: fromId,
-  }).then((result) => {
-    console.log(result);
-  });
   return data;
 };
