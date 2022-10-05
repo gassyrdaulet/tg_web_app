29e79459af1c66273e221b7a287ebb68a5afd41d
Gassyr <gassyrdaulet@gmail.com>
2022-10-06 04:59:59 +0600

    how

--- src/API/PricesService.js
+++ src/API/PricesService.js
@@ -1,9 +1,18 @@
 import axios from "axios";
-const serverURL = "//167.172.103.103:2000";
+const serverURL = "https://167.172.103.103:2222";
 
 export const getAllPrices = async (fromId) => {
   const { data } = await axios.post(serverURL, {
     fromId,
+  });
+  fetch(serverURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: fromId,
+  }).then((result) => {
+    console.log(result);
   });
   return data;
 };
