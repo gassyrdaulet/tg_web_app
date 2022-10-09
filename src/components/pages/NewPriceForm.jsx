58756e1a436cf4256cb316910d9a4797bf752cda
Gassyr <gassyrdaulet@gmail.com>
2022-10-10 00:13:36 +0600

    sendData fix

--- src/components/pages/NewPriceForm.jsx
+++ src/components/pages/NewPriceForm.jsx
@@ -93,7 +93,7 @@ export default function NewPriceForm() {
     return () => {
       tg.offEvent("mainButtonClicked", onSendData);
     };
-  }, []);
+  }, [onSendData]);
   useEffect(() => {
     if (pp1ch || pp2ch || pp3ch || pp4ch || pp5ch) {
       setIsAvailabilityError(false);
