89149e1cee715011e0290be14f50b64cf872a9d1
Gassyr <gassyrdaulet@gmail.com>
2022-11-21 17:28:39 +0600

    d

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import cl from "./styles/Error.module.css";
+import telegram from "../../img/telegram.svg";
 
 export default function Error() {
-  return <div>Error</div>;
+  return (
+    <div className={cl.Error}>
+      <div className={cl.info}>
+        <div className={cl.text}>
+          <h1>Эта страница недоступна в браузере.</h1>
+        </div>
+        <div className={cl.icon}>
+          <img src={telegram} alt="Телеграм Мессенджер." />
+          <a href="https://t.me/KaspiDampingBot">
+            <p>Откройте Telegram.</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
